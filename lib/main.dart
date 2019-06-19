ad20b81fef6ab65dca8c189f05163f681c0cc31a
Massimiliano Dell’Orco <M.DellOrco@almaviva.it>
2019-06-19 18:51:02 +0200

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        title: 'Magic 8 Bar',
+        home: Magic8(),
       ),
     );
+
+class Magic8 extends StatefulWidget {
+  @override
+  _Magic8State createState() => _Magic8State();
+}
+
+class _Magic8State extends State<Magic8> {
+  int ballNumber = 1;
+
+  void getAnswer() {
+    setState(() {
+      ballNumber = Random().nextInt(4) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+      ),
+      body: Center(
+        child: Container(
+          child: FlatButton(
+            child: Image.asset('images/ball$ballNumber.png'),
+            onPressed: () {
+              getAnswer();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
